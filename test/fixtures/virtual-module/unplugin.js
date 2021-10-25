6be9e2408d80d319a828f7a3b9fa84cb52425ed7
Anthony Fu <anthonyfu117@hotmail.com>
2021-10-25 19:32:08 +0800

    test: use slash in virutal modules

--- test/fixtures/virtual-module/unplugin.js
+++ test/fixtures/virtual-module/unplugin.js
@@ -4,13 +4,13 @@ module.exports = createUnplugin(() => {
   return {
     name: 'virtual-module-fixture',
     resolveId (id) {
-      return id.startsWith('virtual-') ? id : null
+      return id.startsWith('virtual/') ? id : null
     },
     load (id) {
-      if (id === 'virtual-1') {
+      if (id === 'virtual/1') {
         return 'export default "VIRTUAL:ONE"'
       }
-      if (id === 'virtual-2') {
+      if (id === 'virtual/2') {
         return 'export default "VIRTUAL:TWO"'
       }
       return null
