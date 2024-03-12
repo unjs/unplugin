6bc8c5c7130a1546c68fac1b5c516d681902a4eb
yuyinws <lyc657508366@gmail.com>
2024-03-12 22:22:19 +0800

    docs: update site link

--- docs/.vitepress/constance.ts
+++ docs/.vitepress/constance.ts
@@ -1,4 +1,4 @@
 export const title = 'Unplugin'
 export const description = 'Unified plugin system. Support Vite, Rollup, webpack, esbuild, and every frameworks on top of them.'
-export const url = 'https://unplugin.vercel.app/'
+export const url = 'https://unplugin.unjs.io/'
 export const ogImage = `${url}/og.png`
