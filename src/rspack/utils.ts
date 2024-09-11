636de4a629aaed4b19626f7b11019595eba4d682
Gengkun <ahabhgk@gmail.com>
2024-09-11 09:46:51 +0800

    fix(rspack): match resource for virtual module (#416)
    * fix: match resource for virtual module
    
    * fix cr
    
    * lint
    
    * Delete .virtual/external-module
    
    ---------
    
    Co-authored-by: Kevin Deng 三咲智子 <sxzz@sxzz.moe>

--- src/rspack/utils.ts
+++ src/rspack/utils.ts
@@ -1,13 +1,39 @@
+import fs from 'fs'
+import { basename, dirname, resolve } from 'path'
+import type { Compiler } from '@rspack/core'
 import type { ResolvedUnpluginOptions } from '../types'
 
 export function encodeVirtualModuleId(id: string, plugin: ResolvedUnpluginOptions): string {
-  return plugin.__virtualModulePrefix + encodeURIComponent(id)
+  return resolve(plugin.__virtualModulePrefix, encodeURIComponent(id))
 }
 
-export function decodeVirtualModuleId(encoded: string, plugin: ResolvedUnpluginOptions): string {
-  return decodeURIComponent(encoded.slice(plugin.__virtualModulePrefix.length))
+export function decodeVirtualModuleId(encoded: string, _plugin: ResolvedUnpluginOptions): string {
+  return decodeURIComponent(basename(encoded))
 }
 
 export function isVirtualModuleId(encoded: string, plugin: ResolvedUnpluginOptions): boolean {
-  return encoded.startsWith(plugin.__virtualModulePrefix)
+  return dirname(encoded) === plugin.__virtualModulePrefix
+}
+
+export class FakeVirtualModulesPlugin {
+  name = 'FakeVirtualModulesPlugin'
+  constructor(private plugin: ResolvedUnpluginOptions) {}
+
+  apply(compiler: Compiler) {
+    const dir = this.plugin.__virtualModulePrefix
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true })
+    }
+    compiler.hooks.shutdown.tap(this.name, () => {
+      if (fs.existsSync(dir)) {
+        fs.rmdirSync(dir, { recursive: true })
+      }
+    })
+  }
+
+  async writeModule(file: string) {
+    const path = encodeVirtualModuleId(file, this.plugin)
+    await fs.promises.writeFile(path, '')
+    return path
+  }
 }
