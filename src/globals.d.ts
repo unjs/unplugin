0ed7e115767b5a5c547fcf5bdf68d013f4932cb5
Kevin Deng 三咲智子 <sxzz@sxzz.moe>
2024-12-11 00:31:37 +0800

    build: use `tsdown` for bundling (#445)
    * feat: switch to tsdown
    
    * upgrade deps
    
    * feat: bundle dts

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -3,6 +3,6 @@
  * __DEV__ is false in the final library output, and it is
  * true when the library is ad-hoc transpiled, ie. during tests.
  *
- * See "tsup.config.ts" and "vitest.config.ts" for more info.
+ * See "tsdown.config.ts" and "vitest.config.ts" for more info.
  */
 declare const __DEV__: boolean
---
+++ tsdown.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: ['src/index.ts', 'src/webpack/loaders/*', 'src/rspack/loaders/*'],
+  format: ['cjs', 'esm'],
+  clean: true,
+  target: 'node18.12',
+  dts: true,
+  sourcemap: false,
+  define: {
+    __DEV__: 'false',
+  },
+  shims: true,
+  external: ['vite', 'webpack', 'rollup', 'esbuild', '@farmfe/core'],
+  unused: { level: 'error' },
+})
