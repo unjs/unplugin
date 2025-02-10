2440b783432e4928491207a3d385a79b3a5ce0be
biodiscus <biodiscus@gmail.com>
2025-02-11 00:16:53 +0100

    feat: switch from picocolors to ansis

--- scripts/buildFixtures.ts
+++ scripts/buildFixtures.ts
@@ -1,8 +1,8 @@
 import { execSync } from 'node:child_process'
 import { join, resolve } from 'node:path'
 import process from 'node:process'
+import c from 'ansis'
 import fs from 'fs-extra'
-import c from 'picocolors'
 
 async function run() {
   const dir = resolve(__dirname, '../test/fixtures')
@@ -16,27 +16,27 @@ async function run() {
     if (fs.existsSync(join(path, 'dist')))
       await fs.remove(join(path, 'dist'))
 
-    console.log(c.yellow(c.inverse(c.bold('\n  Vite  '))), name, '\n')
+    console.log(c.yellow.inverse.bold`\n  Vite  `, name, '\n')
     execSync('npx vite --version', { cwd: path, stdio: 'inherit' })
     execSync('npx vite build', { cwd: path, stdio: 'inherit' })
 
-    console.log(c.red(c.inverse(c.bold('\n  Rollup  '))), name, '\n')
+    console.log(c.red.inverse.bold`\n  Rollup  `, name, '\n')
     execSync('npx rollup --version', { cwd: path, stdio: 'inherit' })
     execSync('npx rollup --bundleConfigAsCjs -c', { cwd: path, stdio: 'inherit' })
 
-    console.log(c.blue(c.inverse(c.bold('\n  Webpack  '))), name, '\n')
+    console.log(c.blue.inverse.bold`\n  Webpack  `, name, '\n')
     execSync('npx webpack --version', { cwd: path, stdio: 'inherit' })
     execSync('npx webpack', { cwd: path, stdio: 'inherit' })
 
-    console.log(c.yellow(c.inverse(c.bold('\n  Esbuild  '))), name, '\n')
+    console.log(c.yellow.inverse.bold`\n  Esbuild  `, name, '\n')
     execSync('npx esbuild --version', { cwd: path, stdio: 'inherit' })
     execSync('node esbuild.config.js', { cwd: path, stdio: 'inherit' })
 
-    console.log(c.cyan(c.inverse(c.bold('\n  Rspack  '))), name, '\n')
+    console.log(c.cyan.inverse.bold`\n  Rspack  `, name, '\n')
     execSync('npx rspack --version', { cwd: path, stdio: 'inherit' })
     execSync('npx rspack', { cwd: path, stdio: 'inherit' })
 
-    console.log(c.magenta(c.inverse(c.bold('\n  Farm  '))), name, '\n')
+    console.log(c.magenta.inverse.bold`\n  Farm  `, name, '\n')
     execSync('npx farm --version', { cwd: path, stdio: 'inherit' })
     execSync('npx farm build', { cwd: path, stdio: 'inherit' })
   }
