4f2f3a2a9e5309e44f70f2544253ed379d101cef
Kevin Deng <sxzz@sxzz.moe>
2026-07-31 22:59:31 +0900

    docs: fix showcase
    Fixes #613

--- docs/.vitepress/data/gen-files.ts
+++ docs/.vitepress/data/gen-files.ts
@@ -7,7 +7,7 @@ import { consola } from 'consola'
 import { $fetch } from 'ofetch'
 import { repositoryMeta } from './meta.ts'
 
-const GITHUB_TOKEN = env.GITHUB_TOKEN
+const { GITHUB_TOKEN } = env
 
 const gql = `#graphql
 query repositoryQuery($owner: String!, $name: String!, $readme: String!) {
@@ -39,49 +39,43 @@ if (!GITHUB_TOKEN) {
   process.exit(1)
 }
 
-const fetchs = repositoryMeta.map((repository) => {
-  return fetchRepo({
-    name: repository.name,
-    owner: repository.owner,
-    readme: repository.defaultBranch ? `${repository.defaultBranch}:README.md` : 'main:README.md',
-  })
-})
+const repos = repositoryMeta.map(repository => fetchRepo({
+  name: repository.name,
+  owner: repository.owner,
+  readme: repository.defaultBranch ? `${repository.defaultBranch}:README.md` : 'main:README.md',
+}))
 
-Promise.allSettled(fetchs).then((res) => {
-  const repoMeta = res?.map((item) => {
-    if (item.status === 'fulfilled') {
-      return {
-        name: item.value?.name,
-        stargazers: item.value?.stargazers,
-        owner: item.value?.owner,
-        description: item.value?.description,
-        url: item.value?.url,
-        isTemplate: item.value?.isTemplate,
-        primaryLanguage: item.value?.primaryLanguage,
-        forkCount: item.value?.forkCount,
-      }
+// eslint-disable-next-line antfu/no-top-level-await
+const res = await Promise.allSettled(repos)
+const repoMeta = res?.map((item) => {
+  if (item.status === 'fulfilled') {
+    return {
+      name: item.value?.name,
+      stargazers: item.value?.stargazers,
+      owner: item.value?.owner,
+      description: item.value?.description,
+      url: item.value?.url,
+      isTemplate: item.value?.isTemplate,
+      primaryLanguage: item.value?.primaryLanguage,
+      forkCount: item.value?.forkCount,
     }
+  }
 
-    return null
-  })?.filter(item => item && item.name)
+  return null
+})?.filter(item => item && item.name)
 
-  writeFileSync(
-    join(dirname(fileURLToPath(import.meta.url)), './repository.json'),
-    JSON.stringify(repoMeta, null, 2),
-  )
-  consola.success('[repository.json] generate success!')
-  consola.success('All files generate done!')
-}).catch((error) => {
-  consola.error(error)
-})
+writeFileSync(
+  join(dirname(fileURLToPath(import.meta.url)), './repository.json'),
+  JSON.stringify(repoMeta, null, 2),
+)
+consola.success('[repository.json] generate success!')
+consola.success('All files generate done!')
 
-async function fetchRepo(meta: {
+async function fetchRepo({ owner, name, readme }: {
   owner: string
   name: string
   readme?: string
 }) {
-  const { owner, name, readme } = meta
-
   const _readme = readme || 'main:README.md'
   try {
     const results = await $fetch('https://api.github.com/graphql', {
@@ -125,6 +119,7 @@ outline: deep
     return repositoryInfo
   }
   catch (error) {
-    consola.error(`[${name}.md]: generate failed: ${error}`)
+    consola.error(`[${name}.md]: generate failed`)
+    throw error
   }
 }
